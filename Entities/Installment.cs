d7a261f6ced0cd1ef0c27cd8a0e7fe9d5dc54c7f
hugosdias <hugo_sdias@hotmail.com>
2023-06-03 15:23:09 -0300

    exercicio de fixacao para interface

---
+++ Entities/Installment.cs
@@ -0,0 +1,23 @@
+using System.Globalization;
+
+namespace contrato
+{
+    public class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+        
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy")
+            + " - "
+            + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Services/IOnlinePaymentService.cs
@@ -0,0 +1,10 @@
+
+
+namespace contrato
+{
+    public interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
